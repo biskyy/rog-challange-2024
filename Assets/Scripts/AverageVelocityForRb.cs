b3cb6a070854ee325173e95e8b4e111bdf8c7b88
biskyy <andreisirbu07@gmail.com>
2024-11-10 20:55:35 +0200

    reformat because ...

--- Assets/Scripts/AverageVelocityForRb.cs
+++ Assets/Scripts/AverageVelocityForRb.cs
@@ -4,40 +4,45 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class AverageVelocityForRb : MonoBehaviour {
-    public Rigidbody rb;
-    public float velocityUpdateInterval = 1f;
-    public TextMeshProUGUI velocityText;
+public class AverageVelocityForRb : MonoBehaviour
+{
+  public Rigidbody rb;
+  public float velocityUpdateInterval = 1f;
+  public TextMeshProUGUI velocityText;
 
-    private Queue<Vector3> positions = new Queue<Vector3>();
-    private Queue<float> timestamps = new Queue<float>();
+  private Queue<Vector3> positions = new Queue<Vector3>();
+  private Queue<float> timestamps = new Queue<float>();
 
-    // Update is called once per frame
-    void Update() {
-        CalculateAverageVelocity();
+  // Update is called once per frame
+  void Update()
+  {
+    CalculateAverageVelocity();
+  }
+
+  void CalculateAverageVelocity()
+  { // made by chatgpt 4o
+    // Record the current position and time
+    positions.Enqueue(rb.position);
+    timestamps.Enqueue(Time.time);
+
+    // Remove old data points that are outside the time window
+    while (timestamps.Count > 0 && Time.time - timestamps.Peek() > velocityUpdateInterval)
+    {
+      positions.Dequeue();
+      timestamps.Dequeue();
     }
 
-    void CalculateAverageVelocity() { // made by chatgpt 4o
-        // Record the current position and time
-        positions.Enqueue(rb.position);
-        timestamps.Enqueue(Time.time);
+    // Calculate the displacement over the time window
+    if (positions.Count > 1)
+    {
+      Vector3 displacement = rb.position - positions.Peek();
+      float timeElapsed = Time.time - timestamps.Peek();
 
-        // Remove old data points that are outside the time window
-        while (timestamps.Count > 0 && Time.time - timestamps.Peek() > velocityUpdateInterval) {
-            positions.Dequeue();
-            timestamps.Dequeue();
-        }
+      // Calculate average velocity
+      Vector3 averageVelocity = displacement / timeElapsed;
 
-        // Calculate the displacement over the time window
-        if (positions.Count > 1) {
-            Vector3 displacement = rb.position - positions.Peek();
-            float timeElapsed = Time.time - timestamps.Peek();
-
-            // Calculate average velocity
-            Vector3 averageVelocity = displacement / timeElapsed;
-
-            // Debug log to see the result
-            velocityText.text = ((int) averageVelocity.magnitude).ToString();
-        }
+      // Debug log to see the result
+      velocityText.text = ((int)averageVelocity.magnitude).ToString();
     }
+  }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,182 +5,209 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class Movement : MonoBehaviour {
-    [Header("Player Properties")]
-    public Vector3 playerScale = new Vector3(1, 1, 1);
-    public float playerDrag = 14f;
-    public float crouchDrag = 20f;
-    public float airDrag = 7f;
-    public float dragSmoothMultiplier = 8000f;
-    private Rigidbody rb;
+public class Movement : MonoBehaviour
+{
+  [Header("Player Properties")]
+  public Vector3 playerScale = new Vector3(1, 1, 1);
+  public float playerDrag = 14f;
+  public float crouchDrag = 20f;
+  public float airDrag = 7f;
+  public float dragSmoothMultiplier = 8000f;
+  private Rigidbody rb;
 
-    [Header("Camera")]
-    public Transform orientation;
+  [Header("Camera")]
+  public Transform orientation;
 
-    [Header("Movement")]
-    public Vector3 moveDirection;
-    public float speed;
-    public float moveSpeed = 8000f;
+  [Header("Movement")]
+  public Vector3 moveDirection;
+  public float speed;
+  public float moveSpeed = 8000f;
 
-    public float fallMultiplier = 3000f;
-    public float airMultiplier = 0.52f;
-    public float crouchAirMultiplier = 0.2f;
+  public float fallMultiplier = 3000f;
+  public float airMultiplier = 0.52f;
+  public float crouchAirMultiplier = 0.2f;
 
-    [Header("Jump")]
-    public float jumpForce = 1000f;
-    public float crouchJumpMultiplier = 0.65f;
+  [Header("Jump")]
+  public float jumpForce = 1000f;
+  public float crouchJumpMultiplier = 0.65f;
 
-    [Header("Crouch")]
-    public float crouchTopSpeed = 13000f;
-    public float crouchSpeed = 5000f;
+  [Header("Crouch")]
+  public float crouchTopSpeed = 13000f;
+  public float crouchSpeed = 5000f;
 
-    [Header("Others")]
-    public Transform feet;
-    public Vector3 groundCheckBox = new Vector3(0.1f, 0.1f, 0.1f);
-    public LayerMask groundLayer;
+  [Header("Others")]
+  public Transform feet;
+  public Vector3 groundCheckBox = new Vector3(0.1f, 0.1f, 0.1f);
+  public LayerMask groundLayer;
 
-    public bool grounded;
-    public bool jumping;
-    public bool crouching;
+  public bool grounded;
+  public bool jumping;
+  public bool crouching;
 
-    private float horizontalInput, verticalInput;
-    public Vector3 slopeMoveDirection;
-    private Vector3 groundNormal;
+  private float horizontalInput, verticalInput;
+  public Vector3 slopeMoveDirection;
+  private Vector3 groundNormal;
 
-    // s tart is called before the first frame update
-    void Start() {
-        rb = GetComponentInChildren<Rigidbody>();
-        rb.freezeRotation = true;
+  // s tart is called before the first frame update
+  void Start()
+  {
+    rb = GetComponentInChildren<Rigidbody>();
+    rb.freezeRotation = true;
 
-        speed = moveSpeed;
+    speed = moveSpeed;
+  }
+
+  // update is called once per frame
+  void Update()
+  {
+    HandleInput();
+    HandleSpeedAndDrag();
+    HandleSlopes();
+
+    grounded = Physics.CheckBox(feet.position, groundCheckBox, Quaternion.Euler(Vector3.down), groundLayer);
+    AdvancedGizmosVisualizer.DisplayBox(feet.position, groundCheckBox, Quaternion.Euler(Vector3.down)); // draw gizmos for ground check
+  }
+
+  void FixedUpdate()
+  {
+    HandleMovement();
+  }
+
+  void HandleInput()
+  {
+    horizontalInput = Input.GetAxisRaw("Horizontal");
+    verticalInput = Input.GetAxisRaw("Vertical");
+
+    moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+    slopeMoveDirection = Vector3.ProjectOnPlane(moveDirection, groundNormal);
+
+    // handle jumping
+    jumping = Input.GetKey(KeyCode.Space);
+
+    if (Input.GetKeyDown(KeyCode.Space) && grounded)
+      Jump();
+
+    // handle crouching
+    crouching = Input.GetKey(KeyCode.LeftShift);
+
+    if (Input.GetKeyDown(KeyCode.LeftShift))
+    {
+      StartCrouch();
+    }
+    if (Input.GetKeyUp(KeyCode.LeftShift))
+    {
+      StopCrouch();
     }
 
-    // update is called once per frame
-    void Update() {
-        HandleInput();
-        HandleSpeedAndDrag();
-        HandleSlopes();
+    if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.R))
+      rb.position = new Vector3(0, 1, 0);
+  }
 
-        grounded = Physics.CheckBox(feet.position, groundCheckBox, Quaternion.Euler(Vector3.down), groundLayer);
-        AdvancedGizmosVisualizer.DisplayBox(feet.position, groundCheckBox, Quaternion.Euler(Vector3.down)); // draw gizmos for ground check
+  void HandleMovement()
+  {
+    if (rb.velocity.y < 0 || GetSlopeMovementDirection() < 0)
+      rb.AddForce(Vector3.down * fallMultiplier * Time.deltaTime);
+
+    if (grounded)
+    {
+      rb.AddForce(slopeMoveDirection * speed * Time.deltaTime);
     }
-
-    void FixedUpdate() {
-        HandleMovement();
+    else if (crouching && !grounded)
+    {
+      rb.AddForce(slopeMoveDirection * speed * crouchAirMultiplier * Time.deltaTime);
     }
-
-    void HandleInput() {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
-
-        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        slopeMoveDirection = Vector3.ProjectOnPlane(moveDirection, groundNormal);
-
-        // handle jumping
-        jumping = Input.GetKey(KeyCode.Space);
-
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
-            Jump();
-
-        // handle crouching
-        crouching = Input.GetKey(KeyCode.LeftShift);
-
-        if (Input.GetKeyDown(KeyCode.LeftShift)) {
-            StartCrouch();
-        }
-        if (Input.GetKeyUp(KeyCode.LeftShift)) {
-            StopCrouch();
-        }
-
-        if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.R))
-            rb.position = new Vector3(0, 1, 0);
+    else
+    {
+      rb.AddForce(slopeMoveDirection * speed * airMultiplier * Time.deltaTime);
     }
+  }
 
-    void HandleMovement() {
-        if (rb.velocity.y < 0 || GetSlopeMovementDirection() < 0)
-            rb.AddForce(Vector3.down * fallMultiplier * Time.deltaTime);
-
-        if (grounded) {
-            rb.AddForce(slopeMoveDirection * speed * Time.deltaTime);
-        } else if (crouching && !grounded) {
-            rb.AddForce(slopeMoveDirection * speed * crouchAirMultiplier * Time.deltaTime);
-        } else {
-            rb.AddForce(slopeMoveDirection * speed * airMultiplier * Time.deltaTime);
-        }
+  void HandleSpeedAndDrag()
+  {
+    if (!grounded)
+    {
+      speed = Mathf.MoveTowards(speed, moveSpeed, Time.deltaTime * dragSmoothMultiplier);
+      rb.drag = airDrag;
     }
-
-    void HandleSpeedAndDrag() {
-        if (!grounded) {
-            speed = Mathf.MoveTowards(speed, moveSpeed, Time.deltaTime * dragSmoothMultiplier);
-            rb.drag = airDrag;
-        } else if (crouching) {
-            if (!IsOnSlope())
-                speed = Mathf.MoveTowards(speed, crouchSpeed, Time.deltaTime * dragSmoothMultiplier);
-            else {
-                if (slopeMoveDirection != Vector3.zero && GetSlopeMovementDirection() < 0) // make sure we are sliding down and not up
-                    speed = Mathf.MoveTowards(speed, speed + 1000f, Time.deltaTime * dragSmoothMultiplier);
-                else {
-                    speed = Mathf.MoveTowards(speed, crouchSpeed, Time.deltaTime * dragSmoothMultiplier);
-                }
-            }
-            if (grounded) // if player crouches mid-air, reset the drag once he touches the ground
-                rb.drag = playerDrag;
-        } else {
-            speed = Mathf.MoveTowards(speed, moveSpeed, Time.deltaTime * dragSmoothMultiplier);
-            rb.drag = playerDrag;
-        }
-    }
-
-    void Jump() {
-        if (crouching)
-            rb.AddForce(rb.transform.up * jumpForce * crouchJumpMultiplier * Time.fixedDeltaTime, ForceMode.Impulse);
+    else if (crouching)
+    {
+      if (!IsOnSlope())
+        speed = Mathf.MoveTowards(speed, crouchSpeed, Time.deltaTime * dragSmoothMultiplier);
+      else
+      {
+        if (slopeMoveDirection != Vector3.zero && GetSlopeMovementDirection() < 0) // make sure we are sliding down and not up
+          speed = Mathf.MoveTowards(speed, speed + 1000f, Time.deltaTime * dragSmoothMultiplier);
         else
-            rb.AddForce(rb.transform.up * jumpForce * Time.fixedDeltaTime, ForceMode.Impulse);
+        {
+          speed = Mathf.MoveTowards(speed, crouchSpeed, Time.deltaTime * dragSmoothMultiplier);
+        }
+      }
+      if (grounded) // if player crouches mid-air, reset the drag once he touches the ground
+        rb.drag = playerDrag;
     }
-
-    void StartCrouch() {
-        // this drag implementation has a logic flaw: you can gain momentum in any direction but you should only gain forward momentum
-        //if (rb.drag >= playerDrag)
-        //    rb.drag = playerDrag / 1.5f; // TODO: apply this only if grounded
-
-        if (speed <= moveSpeed && GetSlopeMovementDirection() <= 0) // prevent spamming (not good detection, must be reworked)
-            speed = crouchTopSpeed;
-
-        // another implementation would be this, however for some reason whatever slide force i give
-        // it, it will always slide for the same amount of time
-        //if (rb.velocity.magnitude > 0.5f)
-        //    rb.AddForce(orientation.transform.forward * slideForce, ForceMode.Force);
-
-        transform.localScale = new Vector3(playerScale.x, playerScale.y / 2, playerScale.z);
-        transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+    else
+    {
+      speed = Mathf.MoveTowards(speed, moveSpeed, Time.deltaTime * dragSmoothMultiplier);
+      rb.drag = playerDrag;
     }
+  }
 
-    void StopCrouch() {
-        transform.localScale = playerScale;
-        transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
-    }
+  void Jump()
+  {
+    if (crouching)
+      rb.AddForce(rb.transform.up * jumpForce * crouchJumpMultiplier * Time.fixedDeltaTime, ForceMode.Impulse);
+    else
+      rb.AddForce(rb.transform.up * jumpForce * Time.fixedDeltaTime, ForceMode.Impulse);
+  }
 
-    private RaycastHit slopeHit;
+  void StartCrouch()
+  {
+    // this drag implementation has a logic flaw: you can gain momentum in any direction but you should only gain forward momentum
+    //if (rb.drag >= playerDrag)
+    //    rb.drag = playerDrag / 1.5f; // TODO: apply this only if grounded
 
-    void HandleSlopes() {
-        Physics.Raycast(feet.position, Vector3.down, out slopeHit, 0.3f);
-        groundNormal = slopeHit.normal;
-        print(groundNormal);
-    }
+    if (speed <= moveSpeed && GetSlopeMovementDirection() <= 0) // prevent spamming (not good detection, must be reworked)
+      speed = crouchTopSpeed;
 
-    bool IsOnSlope() {
-        if (groundNormal != Vector3.up)
-            return true;
-        else
-            return false;
-    }
+    // another implementation would be this, however for some reason whatever slide force i give
+    // it, it will always slide for the same amount of time
+    //if (rb.velocity.magnitude > 0.5f)
+    //    rb.AddForce(orientation.transform.forward * slideForce, ForceMode.Force);
 
-    float GetSlopeMovementDirection() {
-        // calculate the movement direction on the slope
-        Vector3 projectedMovement = Vector3.ProjectOnPlane(moveDirection, groundNormal);
+    transform.localScale = new Vector3(playerScale.x, playerScale.y / 2, playerScale.z);
+    transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+  }
 
-        // return the dot product between the movement direction and the global up vector
-        return Vector3.Dot(projectedMovement, Vector3.up);
-    }
+  void StopCrouch()
+  {
+    transform.localScale = playerScale;
+    transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+  }
+
+  private RaycastHit slopeHit;
+
+  void HandleSlopes()
+  {
+    Physics.Raycast(feet.position, Vector3.down, out slopeHit, 0.3f);
+    groundNormal = slopeHit.normal;
+    print(groundNormal);
+  }
+
+  bool IsOnSlope()
+  {
+    if (groundNormal != Vector3.up)
+      return true;
+    else
+      return false;
+  }
+
+  float GetSlopeMovementDirection()
+  {
+    // calculate the movement direction on the slope
+    Vector3 projectedMovement = Vector3.ProjectOnPlane(moveDirection, groundNormal);
+
+    // return the dot product between the movement direction and the global up vector
+    return Vector3.Dot(projectedMovement, Vector3.up);
+  }
 
 }
