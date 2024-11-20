145003590f111e738e48a7004408d3a149f36b25
biskyy <54770799+biskyy@users.noreply.github.com>
2024-11-20 23:00:47 +0200

    feat: make the SilverKatana do damage to player

--- Assets/Scripts/Enemy/CheckKatanaTouchedPlayer.cs
+++ Assets/Scripts/Enemy/CheckKatanaTouchedPlayer.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class CheckKatanaTouchedPlayer : MonoBehaviour
 {
   public SilverKatana silverKatana;
+  public Player player;
+  public bool playerDidAnythingToStopAttack = false;
+  public bool playerParried = false;
+
   // Start is called before the first frame update
   void Start()
   {
@@ -23,15 +27,50 @@ public class CheckKatanaTouchedPlayer : MonoBehaviour
     if (collider.tag == "player")
     {
       silverKatana.playerTouched = true;
-      silverKatana.player = collider.GetComponentInParent<Player>();
+      player = collider.GetComponentInParent<Player>();
+      silverKatana.player = player;
+
+      if (player.blocking)
+        playerDidAnythingToStopAttack = true;
+      if (player.redKatana.parried)
+        playerParried = true;
+      print(collider);
     }
   }
+
+  void OnTriggerStay(Collider collider)
+  {
+    if (collider.tag == "player")
+    {
+      if (player.blocking)
+        playerDidAnythingToStopAttack = true;
+      if (player.redKatana.parried)
+        playerParried = true;
+    }
+  }
+
 
   void OnTriggerExit(Collider collider)
   {
     if (collider.tag == "player")
     {
+
+      if (player.blocking)
+        playerDidAnythingToStopAttack = true;
+      if (player.redKatana.parried)
+        playerParried = true;
+
+      // do nothing
+      if (playerParried) { }
+      else if (playerDidAnythingToStopAttack)
+        player.TakeDamage(silverKatana.damage / 2f);
+      else
+        player.TakeDamage(silverKatana.damage);
+
+      playerDidAnythingToStopAttack = false;
+      playerParried = false;
       silverKatana.playerTouched = false;
     }
+
   }
 }
