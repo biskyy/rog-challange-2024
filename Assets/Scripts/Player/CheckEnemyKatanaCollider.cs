395eba287871493b0cb143bc5918be78e61cd955
biskyy <andreisirbu07@gmail.com>
2024-11-20 10:33:20 +0200

    config: use GetComponent<>()

--- Assets/Scripts/Player/CheckEnemyKatanaCollider.cs
+++ Assets/Scripts/Player/CheckEnemyKatanaCollider.cs
@@ -10,7 +10,7 @@ public class CheckEnemyKatanaCollider : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
-
+    redKatana = GetComponentInParent<RedKatana>();
   }
 
   // Update is called once per frame
