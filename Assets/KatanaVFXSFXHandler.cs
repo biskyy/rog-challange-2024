727697c2018d345993ff93b641295d7e0483a23c
biskyy <54770799+biskyy@users.noreply.github.com>
2024-11-27 23:34:46 +0200

    feat: play a trail vfx on attack

---
+++ Assets/KatanaVFXSFXHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KatanaVFXSFXHandler : MonoBehaviour
+{
+  public RedKatana katana;
+    // Start is called before the first frame update
+    void Start()
+    {
+        katana = GetComponentInChildren<RedKatana>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+  public void EnableTrailVFX()
+  {
+    katana.trailVFX.Play();
+  }
+
+  public void DisableTrailVFX()
+  {
+    katana.trailVFX.Clear();
+    katana.trailVFX.Stop();
+  }
+}
