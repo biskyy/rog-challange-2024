c2aca7874442e1c1181f844bdbb444daf5288af9
biskyy <54770799+biskyy@users.noreply.github.com>
2024-11-29 14:32:11 +0200

    feat: implement stun method into target dummy

--- Assets/Scripts/TargetDummy.cs
+++ Assets/Scripts/TargetDummy.cs
@@ -8,7 +8,9 @@ public class TargetDummy : MonoBehaviour
   public Transform player;
   public Transform orientation;
   public float radiusCheck = 10f;
-  public Animator animator;
+  public Animator enemyKatanaAnimator;
+  public ParticleSystem stunVFX;
+  public bool stunned = false;
 
   // Start is called before the first frame update
   void Start()
@@ -20,14 +22,14 @@ public class TargetDummy : MonoBehaviour
   void Update()
   {
     bool playerNearby = Physics.CheckSphere(transform.position, radiusCheck, playerLayer);
-    if (playerNearby)
+    if (playerNearby && !stunned)
     {
-      animator.SetBool("enemySpotted", true);
+      enemyKatanaAnimator.SetBool("enemySpotted", true);
       orientation.LookAt(player);
     }
     else
     {
-      animator.SetBool("enemySpotted", false);
+      enemyKatanaAnimator.SetBool("enemySpotted", false);
     }
   }
 
@@ -35,5 +37,22 @@ public class TargetDummy : MonoBehaviour
   {
     Gizmos.color = new Color(1, 1, 1, 0.5f);
     Gizmos.DrawSphere(transform.position, radiusCheck);
+  }
+
+  public void GetStunned()
+  {
+    StartCoroutine(Stun());
+  }
+
+  private IEnumerator Stun()
+  {
+    yield return new WaitForSeconds(0.2f);
+    enemyKatanaAnimator.speed = 1f;
+    stunned = true;
+    stunVFX.Play();
+    yield return new WaitForSeconds(3f);
+    stunned = false;
+    stunVFX.Stop();
+    stunVFX.Clear();
   }
 }
